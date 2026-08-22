b04aec362d96c455c34de8790724738bab392376
snowlyr <snow.lyr3@gmail.com>
2026-08-22 14:32:16 +0800

    Level-2. Add, List

--- src/main/java/Shan.java
+++ src/main/java/Shan.java
@@ -7,6 +7,8 @@ public class Shan {
       + "\\___ \\| '_ \\ / _` | '_ \\\n"
       + " ___) | | | | (_| | | | |\n"
       + "|____/|_| |_|\\__,_|_| |_| \n";
+  private static String[] taskList = new String[100];
+  private static int taskIdx = 0;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -20,8 +22,12 @@ public class Shan {
     System.out.print("> ");
     while (sc.hasNextLine()) {
       String command = sc.nextLine();
-      sendMessage(reply(command));
 
+      switch (command) {
+        case "bye" -> sendMessage(commandExit());
+        case "list" -> sendMessage(commandList());
+        default -> sendMessage(commandAdd(command));
+      }
       if (command.equals("bye")) {
         break;
       }
@@ -29,20 +35,6 @@ public class Shan {
     }
 
     sc.close();
-  }
-
-  /**
-   * Determines Shan's response to a command
-   *
-   * @param command command entered by the user
-   * @return farewell message for (@code bye), or the original command otherwise
-   */
-  private static String reply(String command) {
-    if (command.equals("bye")) {
-      return "Bye. Hope to see you again soon!!!";
-    }
-
-    return command;
   }
 
   /**
@@ -54,5 +46,39 @@ public class Shan {
     System.out.println(DIVIDER);
     System.out.println("Shan: " + message);
     System.out.println(DIVIDER);
+  }
+
+  /**
+   * Return farewell message
+   *
+   * @return farewell message for (@code bye)
+   */
+  private static String commandExit() {
+    return "Bye. Hope to see you again soon!!!";
+  }
+
+  /**
+   * Add a task to taskList
+   *
+   * @param task task to add
+   * @return reply when adding task
+   */
+  private static String commandAdd(String task) {
+    taskList[taskIdx] = task;
+    taskIdx++;
+    return String.format("added: %s", task);
+  }
+
+  /**
+   * Returns the list of tasks
+   *
+   * @return enumerated list of tasks
+   */
+  private static String commandList() {
+    String res = "\n";
+    for (int i = 0; i < taskIdx; i++) {
+      res = res + String.format("%d. %s\n", i + 1, taskList[i].toString());
+    }
+    return res.substring(0, res.length() - 1);
   }
 }
